05976439274c5b213a821ecf84209ccdc8d4f71e
Diyor Gaffarov <diyorgaffarov@gmail.com>
2026-04-13 01:59:05 +0500

    opimized code for dashboard and saved separete result.csv file

--- dashboard.py
+++ dashboard.py
@@ -4,13 +4,19 @@ import pandas as pd
 import plotly.graph_objects as go
 
 
-def render_tab(orders_path, users_path, books_path, year_key, month_key):
+def render_tab(orders_path, books_path, results_path, year_key, month_key):
     orders_df = pd.read_csv(orders_path)
-    users_df = pd.read_csv(users_path)
     books_df = pd.read_csv(books_path)
+    results = pd.read_csv(results_path).iloc[0]
+
+    unique_users = results['unique_users']
+    unique_set_of_authors = results['unique_set_of_authors']
+    author_name = results['author_name']
+    author_sold = results['author_sold']
+    best_buyer_ids = results['best_buyer_ids']
+    total_spent = results['total_spent']
 
     orders_df['date'] = pd.to_datetime(orders_df['date'])
-
     daily_revenue = orders_df.groupby('date')['paid_price'].sum().reset_index()
     daily_revenue.columns = ['date', 'revenue']
 
@@ -20,54 +26,21 @@ def render_tab(orders_path, users_path, books_path, year_key, month_key):
 
     top5_fig = go.Figure(data=[go.Table(
         columnwidth=[30, 150, 150],
-        header=dict(values=['#', 'Date', 'Revenue $'], fill_color='#2c3e50', font=dict(color='white', size=14), align='center'),
-        cells=dict(values=[top5['rank'], top5['date'], top5['revenue']], fill_color=[['#f9f9f9', '#ffffff'] * 3], font=dict(size=13), align='center', height=35)
+        header=dict(
+            values=['#', 'Date', 'Revenue $'],
+            fill_color='#2c3e50',
+            font=dict(color='white', size=14),
+            align='center'
+        ),
+        cells=dict(
+            values=[top5['rank'].tolist(), top5['date'].tolist(), top5['revenue'].tolist()],
+            fill_color='white',
+            font=dict(size=13, color='black'),
+            align='center',
+            height=35
+        )
     )])
     top5_fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), height=220)
-
-    parent = {i: i for i in users_df.index}
-
-    def find(x):
-        while parent[x] != x:
-            parent[x] = parent[parent[x]]
-            x = parent[x]
-        return x
-
-    def union(x, y):
-        parent[find(x)] = find(y)
-
-    def is_same_user(r1, r2):
-        matches = (
-            (r1['name'] == r2['name']) +
-            (r1['address'] == r2['address']) +
-            (r1['phone'] == r2['phone']) +
-            (r1['email'] == r2['email'])
-        )
-        return matches >= 3
-
-    records = users_df.reset_index().to_dict('records')
-    for i in range(len(records)):
-        for j in range(i + 1, len(records)):
-            if is_same_user(records[i], records[j]):
-                union(records[i]['index'], records[j]['index'])
-
-    unique_users = len(set(find(i) for i in users_df.index))
-    unique_set_of_authors = books_df['author'].nunique()
-
-    merged_books = orders_df.merge(books_df, left_on='book_id', right_on='id')
-    author_sales = merged_books.groupby('author')['quantity'].sum().reset_index()
-    author_sales.columns = ['author', 'total_sold']
-    most_popular = author_sales.nlargest(1, 'total_sold')
-    author_name = most_popular['author'].values[0]
-    author_sold = most_popular['total_sold'].values[0]
-
-    users_df['user_group'] = [find(i) for i in users_df.index]
-    merged_users = orders_df.merge(users_df[['id', 'user_group']], left_on='user_id', right_on='id')
-    spending = merged_users.groupby('user_group')['paid_price'].sum().reset_index()
-    spending.columns = ['user_group', 'total_spent']
-    top_group = spending.nlargest(1, 'total_spent')['user_group'].values[0]
-    best_buyer_ids = users_df[users_df['user_group'] == top_group]['id'].tolist()
-    total_spent = spending[spending['user_group'] == top_group]['total_spent'].values[0]
 
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -87,7 +60,7 @@ def render_tab(orders_path, users_path, books_path, year_key, month_key):
                 <p style="font-size:20px; font-weight:bold; margin:5px 0">{best_buyer_ids}</p>
                 <hr style="border:0.5px solid #eeeeee">
                 <p style="color:gray; font-size:13px; margin:0">Total Spent</p>
-                <p style="font-size:20px; font-weight:bold; margin:5px 0">${total_spent:,.2f}</p>
+                <p style="font-size:20px; font-weight:bold; margin:5px 0">${float(total_spent):,.2f}</p>
             </div>
         """, unsafe_allow_html=True)
     with col3:
@@ -132,10 +105,10 @@ st.divider()
 data1_tab, data2_tab, data3_tab = st.tabs(["DATA1", "DATA2", "DATA3"])
 
 with data1_tab:
-    render_tab('DATA1/data1/orders_clean.csv', 'DATA1/data1/users_clean.csv', 'DATA1/data1/books_clean.csv', 'year1', 'month1')
+    render_tab('DATA1/data1/orders_clean.csv', 'DATA1/data1/books_clean.csv', 'DATA1/data1/results.csv', 'year1', 'month1')
 
 with data2_tab:
-    render_tab('DATA2/data2/orders_clean.csv', 'DATA2/data2/users_clean.csv', 'DATA2/data2/books_clean.csv', 'year2', 'month2')
+    render_tab('DATA2/data2/orders_clean.csv', 'DATA2/data2/books_clean.csv', 'DATA2/data2/results.csv', 'year2', 'month2')
 
 with data3_tab:
-    render_tab('DATA3/data3/orders_clean.csv', 'DATA3/data3/users_clean.csv', 'DATA3/data3/books_clean.csv', 'year3', 'month3')
+    render_tab('DATA3/data3/orders_clean.csv', 'DATA3/data3/books_clean.csv', 'DATA3/data3/results.csv', 'year3', 'month3')
